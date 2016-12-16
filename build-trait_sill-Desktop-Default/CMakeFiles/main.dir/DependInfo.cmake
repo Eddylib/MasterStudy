927f52dece9187c1805c69eeae265642b62a0c94
Eddylib <libaoyuxd@gmail.com>
2016-12-16 08:36:36 +0800

    a

--- build-trait_sill-Desktop-Default/CMakeFiles/main.dir/DependInfo.cmake
+++ build-trait_sill-Desktop-Default/CMakeFiles/main.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ SET(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 SET(CMAKE_DEPENDS_CHECK_CXX
-  "/home/libaoyu/qtcreater_ws/learn_cpp_base/trait_sill/main.cpp" "/home/libaoyu/qtcreater_ws/learn_cpp_base/build-trait_sill-Desktop-Default/CMakeFiles/trait_sill.dir/main.cpp.o"
+  "/home/libaoyu/qtcreater_ws/learn_cpp_base/trait_sill/src/main_traits.cpp" "/home/libaoyu/qtcreater_ws/learn_cpp_base/build-trait_sill-Desktop-Default/CMakeFiles/main.dir/src/main_traits.cpp.o"
   )
 SET(CMAKE_CXX_COMPILER_ID "GNU")
 
@@ -14,7 +14,7 @@ SET(CMAKE_TARGET_LINKED_INFO_FILES
 
 # The include file search paths:
 SET(CMAKE_C_TARGET_INCLUDE_PATH
-  "/home/libaoyu/qtcreater_ws/learn_cpp_base/trait_sill/."
+  "/home/libaoyu/qtcreater_ws/learn_cpp_base/trait_sill/./include"
   )
 SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
 SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
